ae796d131b7d692bd737197c9789493798af2d2d
HypeR <hyperru1lvl@gmail.com>
2022-05-15 01:33:29 +0300

    added automatons output info window

--- include/connector_gi.h
+++ include/connector_gi.h
@@ -1,6 +1,7 @@
 #ifndef CONNECTOR_GI_H
 #define CONNECTOR_GI_H
 
+#include <QGraphicsProxyWidget>
 #include <QGraphicsItem>
 
 class AutomatonGI;
@@ -12,11 +13,18 @@ public:
     ~ConnectorGI() = default;
 
     QRectF boundingRect() const;
+protected:
+     void hoverEnterEvent( QGraphicsSceneHoverEvent *event );
+     void hoverLeaveEvent( QGraphicsSceneHoverEvent *event );
+     void mousePressEvent( QGraphicsSceneMouseEvent *event );
 
+     void setup_output();
 private:
-    void paint( QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget );
+     void paint( QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget );
 
-    AutomatonGI* automaton_;
+     AutomatonGI* automaton_;
+     QGraphicsProxyWidget* output_info_ = nullptr;
+     bool output_fixed_ = false;
 };
 
 #endif // CONNECTOR_GI_H
--- src/connector_gi.cpp
+++ src/connector_gi.cpp
@@ -1,10 +1,16 @@
 #include <automaton_gi.h>
 #include <connector_gi.h>
 #include <QPainter>
+#include <QGraphicsScene>
+#include <QGroupBox>
+#include <QVBoxLayout>
+#include <QLabel>
 
 ConnectorGI::ConnectorGI( AutomatonGI* automaton ):
      automaton_( automaton )
-{}
+{
+     setAcceptHoverEvents(true);
+}
 
 
 QRectF ConnectorGI::boundingRect() const
@@ -18,5 +24,46 @@ void ConnectorGI::paint( QPainter *painter, const QStyleOptionGraphicsItem *, QW
      painter->setPen( Qt::black );
      painter->setBrush( Qt::blue );
      painter->drawEllipse( -15, -15, 30, 30 );
+}
+
+void ConnectorGI::setup_output()
+{
+     QGroupBox* gb = new QGroupBox( "Output:" );
+     gb->setFixedWidth( 200 );
+     QVBoxLayout* layout = new QVBoxLayout();
+     QLabel* info = new QLabel( gb );
+     info->setText( "Automaton " + QString::number( automaton_->get_automaton_index() ) + " output" );
+     layout->addWidget( info );
+     gb->setLayout( layout );
+
+     output_info_ = scene()->addWidget( gb );
+     output_info_->setPos(
+          scenePos().x() - 100, scenePos().y() + 20
+     );
+}
+
+
+void ConnectorGI::hoverEnterEvent( QGraphicsSceneHoverEvent *event )
+{
+     if ( output_info_ )
+          return;
+     setup_output();
+}
+
+
+void ConnectorGI::hoverLeaveEvent( QGraphicsSceneHoverEvent *event )
+{
+     if ( !output_fixed_ && output_info_ && scene() )
+     {
+          scene()->removeItem( output_info_ );
+          delete output_info_;
+          output_info_ = nullptr;
+     }
+}
+
+
+void ConnectorGI::mousePressEvent( QGraphicsSceneMouseEvent *event )
+{
+     output_fixed_ = !output_fixed_;
 }
 
